f3f102ffaab582fa4ea3b18c50a282857aca3ec6
Ted Vu <tedvu184@gmail.com>
2020-08-02 15:45:25 +1000

    Set up base application

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AboutPage = () => (
+  <div>
+    <h2>About page</h2>
+    <p>
+      This app uses React, Redux and React Router and many other helpful
+      libraries
+    </p>
+  </div>
+);
+
+export default AboutPage;
